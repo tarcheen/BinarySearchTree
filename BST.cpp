c7d25bbfb0aff1a63a41d075d72c235502f58da5
hamedmirlohi <mirlohi@me.com>
2019-06-25 17:26:54 -0700

    left to right sorted implemented

--- BST.cpp
+++ BST.cpp
@@ -31,7 +31,7 @@ void disp_tree_leaf_only(node* root)
 		disp_tree_leaf_only(root->right);
 }
 
-void disp_left_to_right(node* root)
+void disp_left_to_right_sorted(node* root)
 {
 	if (root == nullptr)
 	{
@@ -39,18 +39,19 @@ void disp_left_to_right(node* root)
 		return;
 	}
 
-	if (root->left == nullptr)
+	if (root->left == nullptr && root->right == nullptr)
 	{
 		cout << root->data << endl;
 		return;
 	}
 
-	disp_left_to_right(root->left);
+	if(root->left != nullptr)
+		disp_left_to_right_sorted(root->left);
 
 	cout << root->data << endl;
 
 	if (root->right != nullptr)
-		disp_left_to_right(root->right);
+		disp_left_to_right_sorted(root->right);
 
 }
 
@@ -90,11 +91,13 @@ int main()
 	root = insert_tree(root, 25);
 	root = insert_tree(root, 7);
 	root = insert_tree(root, 11);
+	root = insert_tree(root, 9);
+	root = insert_tree(root, 8);
 
 	cout << "Display Leafs Only: " << endl;
 	disp_tree_leaf_only(root);
 
 	cout << "Display Tree Left to Right" << endl;
-	disp_left_to_right(root);
+	disp_left_to_right_sorted(root);
 
 }
